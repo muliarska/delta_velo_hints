683e886c7f6066e6bcd420b90e62ea1ccc18b0e1
Slavko Prytula <slavko.prytula@gmail.com>
2021-05-06 16:48:05 +0300

    new leaflet component

--- src/Map.js
+++ src/Map.js
@@ -1,41 +1,322 @@
-import React, { Component } from "react";
-import { Map, TileLayer, withLeaflet, MapControl } from "react-leaflet";
+import React, { Component, useState, useEffect, useRef } from "react";
+import { Map, TileLayer, withLeaflet, MapControl, Marker, Popup, LeafletConsumer } from "react-leaflet";
 import MapInfo from "./MapInfo";
 import Routing from "./RoutingMachine";
+import L from "leaflet";
+import "leaflet-routing-machine";
+import './Icons'
+
+
+const accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+
+
+var routing = false;
+var markers = [];
+var control;
+var map = NaN;
+
+var route = NaN;
+
+var modal = document.getElementById("myModal");
+var directionsStart = document.getElementById("directionsStart");
+var span = document.getElementsByClassName("close")[0];
+
+var badRoadReportButton = document.getElementById("badRoadReportButton");
+var accidentReportButton = document.getElementById("accidentReportButton");
+var parkingPlaceReportButton = document.getElementById("parkingPlaceReportButton");
+var closeButtonReport = document.getElementById("closeButtonReport");
+
+
+var cancelRouteButton = document.getElementById('cancelRouteButton');
+var startMenuButton = document.getElementById('startMenuButton');
+
+
+var startNewRouteButton = document.getElementById("startNewRouteButton");
+var closeButton = document.getElementById("closeButton");
+
+
+function onLoad(e) {
+  // map.setView([coordinates], 8);
+  loadDirectionsStart();
+}
+
+function loadDirectionsStart (e) {
+  directionsStart.style.display = "block";
+}
+
+function startMenu() {
+  console.log("starting menu");
+  cancelRouteButton.style.display = 'none';
+  startMenuButton.style.display = 'none';
+  loadDirectionsStart();
+}
+
+function cancelRoute(layer) {
+  console.log("canceling route");
+  cancelRouteButton.style.display = 'none';
+  startMenuButton.style.display = 'inline-block';
+  removeRoute(route)
+}
+
+
+
+function makeReport(markers) {
+  var response = "";
+
+  modal.style.display = "block";
+
+  badRoadReportButton.onclick = function(event) {
+    modal.style.display = "none";
+    console.log("badRoadReportButton")
+    response = "badRoadReportButton"
+    // marker.bindPopup("Bad Road");
+    // marker.setIcon(badRoadIcon);
+  }
+
+  accidentReportButton.onclick = function(event) {
+    modal.style.display = "none";
+    console.log("accidentReportButton")
+    response = "accidentReportButton"
+    // marker.bindPopup("Accident");
+    // marker.setIcon(accidentIcon);
+  }
+
+  parkingPlaceReportButton.onclick = function(event) {
+    modal.style.display = "none";
+    console.log("parkingPlaceReportButton")
+    response = "parkingPlaceReportButton"
+    // marker.bindPopup("Parking Place");
+    // marker.setIcon(parkingIcon);
+  }
+
+  closeButtonReport.onclick = function(event) {
+    modal.style.display = "none";
+    console.log("Closing")
+    markers.pop();
+    startMenuButton.style.display = 'inline-block';
+  }
+
+  return response;
+}
+
+
+startNewRouteButton.onclick = function(event) {
+  directionsStart.style.display = "none";
+  console.log("Starting new route");
+  routing = true;
+  cancelRouteButton.style.display = 'inline-block';
+  startMenuButton.style.display = 'none';
+}
+
+closeButton.onclick = function(event) {
+  directionsStart.style.display = "none";
+  console.log("Closing start screen")
+  startMenuButton.style.display = 'inline-block';
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  directionsStart.style.display = "none";
+  startMenuButton.style.display = 'inline-block';
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    startMenuButton.style.display = 'inline-block';
+  }
+  if (event.target == directionsStart) {
+    directionsStart.style.display = "none";
+    startMenuButton.style.display = 'inline-block';
+  }
+}
+
+
+
+startMenuButton.onclick = function(event) {
+  startMenu();
+}
+
+cancelRouteButton.onclick = function(event) {
+  cancelRoute(map);
+}
+
+
+function createRoute(markers) {
+  let leafletElement = L.Routing.control({
+    router: L.Routing.mapbox(accessToken, {
+      unit: 'metric',
+      profile : 'mapbox/cycling',
+      language: 'en',
+      alternatives: true,
+      geometries: 'geojson',
+      controls: { instructions: false },
+      flyTo: false
+    }),
+    waypoints: [
+      L.latLng(markers[0]['position']),
+      L.latLng(markers[1]['position'])
+    ],
+    lineOptions: {
+      styles: [
+        {
+          color: "gray",
+          opacity: 0.7,
+          weight: 6
+        }
+      ]
+    },
+    addWaypoints: false,
+    draggableWaypoints: false,
+    fitSelectedRoutes: false,
+    showAlternatives: true
+  }).addTo(map.leafletElement);
+  leafletElement.hide();
+
+  // handleFlyTo();
+  // function handleFlyTo() {
+  //   const { current = {} } = useRef();
+  //   const { leafletElement: map } = current;
+  //   map.flyTo(markers[0]['position'], 14);
+  // }
+
+  routing = false;
+  // .flyTo(markers[0]['position']);
+
+  return leafletElement;
+}
+
+
+function removeRoute(route) {
+  route.remove();
+}
+
+
+function getIcon(name) {
+  return L.icon({
+    iconUrl: require('./marker_icons/' + name + '.png'),
+    iconSize: [40]
+  })
+}
+
 
 class MapComponent extends React.Component {
   constructor() {
     super();
-    this.state = {
-      lat: 17.4,
-      lng: 78.4,
-      zoom: 7,
-      isMapInit: false
-    };
-  }
+    this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
 
-  saveMap = (map) => {
-    this.map = map;
+    this.state = {
+      lat: 49.918313,
+      lng: 24.182573,
+      zoom: 8,
+      isMapInit: false,
+
+      markers: [],
+      routing_markers: []
+
+    };
+  };
+  
+  saveMap = (layer) => {
+    map = layer;
     this.setState({
       isMapInit: true
     });
+
+    onLoad();
   };
+
+  forceUpdateHandler(){
+    this.forceUpdate();
+  };
+
+
+  addMarker = (e) => {
+    // clearGeoResults();
+    console.log('clicked')
+    var coordinates = e.latlng
+    
+    if (!routing) {
+      const {markers} = this.state
+      
+      const resp = makeReport(markers);
+      this.forceUpdateHandler();
+      console.log(resp)
+      markers.push({
+        'name': 'marker', 
+        'position': coordinates
+      })
+      this.setState({markers})
+      
+    } else {
+      const {routing_markers} = this.state
+
+      routing_markers.push({
+        'name': 'route-mark', 
+        'position': coordinates
+      })
+      
+      if (routing_markers.length == 2) {
+        route = createRoute(routing_markers);
+      }
+      this.setState({routing_markers})
+    }
+  }
+
+
 
   render() {
     const { lat, lng, zoom } = this.state;
     const position = [lat, lng];
 
     return (
-      <Map center={position} zoom={zoom} ref={this.saveMap}>
+      <Map className='map'
+        center={position} 
+        zoom={zoom} 
+
+        onClick={this.addMarker} 
+        ref={this.saveMap}>
+
         <TileLayer
           // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          url="https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2xhdmtvcHJ5dHVsYSIsImEiOiJja281cnRmdjQwODl5MnZwZGdmeW1hb3N3In0.mhPtHgPEkyIHhGCQcNJNlA"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2xhdmtvcHJ5dHVsYSIsImEiOiJja281cnRmdjQwODl5MnZwZGdmeW1hb3N3In0.mhPtHgPEkyIHhGCQcNJNlA"
         />
-        {this.state.isMapInit && <Routing map={this.map} />}
+
+        {this.state.markers.map((location, idx) => (
+          <Marker 
+            key={`marker-${idx}`} 
+            position={location.position} 
+            // icon={getIcon(location.name)}
+            >
+              
+            <Popup>
+              {location.name}
+            </Popup>
+          </Marker>
+        ))}
+
+
+        {this.state.routing_markers.map((location, idx) => (
+          <Marker 
+            key={`marker-${idx}`} 
+            position={location.position} 
+            // icon={getIcon(location.name)}
+            >
+              
+            <Popup>
+              {location.name}
+            </Popup>
+          </Marker>
+        ))}
+
       </Map>
     );
   }
 }
+
+
+
 
 export default MapComponent;
--- src/RoutingMachine.js
+++ src/RoutingMachine.js
@@ -1,34 +1,48 @@
-import { MapLayer } from "react-leaflet";
-import L from "leaflet";
-import "leaflet-routing-machine";
-// import "lrm-google";
-import { withLeaflet } from "react-leaflet";
+// import { MapLayer } from "react-leaflet";
+// import L from "leaflet";
+// import "leaflet-routing-machine";
+// // import "lrm-google";
+// import { withLeaflet } from "react-leaflet";
 
-class Routing extends MapLayer {
-  createLeafletElement() {
-    const { map } = this.props;
-    let leafletElement = L.Routing.control({
-      waypoints: [
-        L.latLng(16.506, 80.648),
-        L.latLng(17.384, 80.4866),
-        L.latLng(12.971, 77.5945)
-      ],
-      // router: new L.Routing.Google(),
-      lineOptions: {
-        styles: [
-          {
-            color: "blue",
-            opacity: 0.6,
-            weight: 4
-          }
-        ]
-      },
-      addWaypoints: false,
-      draggableWaypoints: true,
-      fitSelectedRoutes: false,
-      showAlternatives: true
-    }).addTo(map.leafletElement);
-    return leafletElement.getPlan();
-  }
-}
-export default withLeaflet(Routing);
+
+// const accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+
+// class Routing extends MapLayer {
+//   createLeafletElement() {
+//     const { map } = this.props;
+//     let leafletElement = L.Routing.control({
+//       router: L.Routing.mapbox(accessToken, {
+//         unit: 'metric',
+//         profile : 'mapbox/cycling',
+//         language: 'en',
+//         alternatives: true,
+//         geometries: 'geojson',
+//         controls: { instructions: false },
+//         flyTo: false
+//       }),
+//       waypoints: [
+//         L.latLng(16.506, 80.648),
+//         L.latLng(17.384, 80.4866),
+//         L.latLng(12.971, 77.5945)
+//       ],
+//       // router: new L.Routing.Google(),
+//       lineOptions: {
+//         styles: [
+//           {
+//             color: "gray",
+//             opacity: 0.7,
+//             weight: 6
+//           }
+//         ]
+//       },
+//       addWaypoints: false,
+//       draggableWaypoints: true,
+//       fitSelectedRoutes: false,
+//       showAlternatives: true
+//     }).addTo(map.leafletElement);
+//     leafletElement.hide();
+
+//     return leafletElement.getPlan();
+//   }
+// }
+// export default withLeaflet(Routing);
